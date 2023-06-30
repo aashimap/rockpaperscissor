3968a4d893b0d2eca57497a33e5f3039b9de49c2
aashimap <aashima@bititude.com>
2023-06-30 09:22:17 +0530

    Update

--- javascript.js
+++ javascript.js
@@ -1,65 +1,67 @@
-console.log('loading')
-const option = document.querySelectorAll('div.options button');
-const result = document.querySelector('#resulttext');
-const playerScore = document.querySelector('#playerscore');
-const computerScore = document.querySelector('#computerscore');
-const reset = document.querySelector('#reset');
+console.log("loading");
 
-reset.addEventListener('click',() => location.reload());
+const option = document.querySelectorAll("div.options button");
+const result = document.querySelector("#resulttext");
+const playerScore = document.querySelector("#playerscore");
+const computerScore = document.querySelector("#computerscore");
+const reset = document.querySelector("#reset");
 
-option.forEach(button => { button.addEventListener('click', getPlayerOption) });
+reset.addEventListener("click", () => location.reload());
 
-let computerOptions = [{option: 'Rock', value: 0}, {option: 'Paper', value: 1}, {option: 'Scissors', value: 2}];
+option.forEach((button) => {
+  button.addEventListener("click", getPlayerOption);
+});
+
+let computerOptions = [
+  { option: "Rock", value: 0 },
+  { option: "Paper", value: 1 },
+  { option: "Scissors", value: 2 },
+];
 let pScore = 0;
 let cScore = 0;
 let playerOption;
 
 function computerPlay() {
-    let result = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    return result;
-  }
+  let result =
+    computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  return result;
+}
 
 function game(playerSelect, computerSelect) {
-
-    if (Number(playerSelect) === computerSelect.value) {
-        playerScore.textContent = ++pScore;
-        computerScore.textContent = ++cScore;
-        result.textContent = "Tie!";
-       }
-    else if (Number(playerSelect) > computerSelect.value) {
-        playerScore.textContent = ++pScore;
-        result.textContent = `You win! ${playerOption} beats ${computerSelect.option}`;
-       }
-    else {
-        computerScore.textContent = ++cScore;
-        result.textContent = `You lose! ${computerSelect.option} beats ${playerOption}`;
-       }
+  if (Number(playerSelect) === computerSelect.value) {
+    playerScore.textContent = ++pScore;
+    computerScore.textContent = ++cScore;
+    result.textContent = "Tie!";
+  } else if (Number(playerSelect) > computerSelect.value) {
+    playerScore.textContent = ++pScore;
+    result.textContent = `You win! ${playerOption} beats ${computerSelect.option}`;
+  } else {
+    computerScore.textContent = ++cScore;
+    result.textContent = `You lose! ${computerSelect.option} beats ${playerOption}`;
+  }
   check();
- }
+}
 
 function check() {
-    if (cScore === 5 || pScore === 5) {
-      if (cScore === pScore){
-        result.textContent = "Tie";
-        }
-      if (pScore > cScore) {
-            result.textContent = "You win the game";
-        }
-      if (cScore > pScore) {
-            result.textContent = "You lose the game";
-        }
-    option.forEach(button => {
-              button.removeEventListener('click', getPlayerOption);
-            });
-
-         }
+  if (cScore === 5 || pScore === 5) {
+    if (cScore === pScore) {
+      result.textContent = "Tie";
+    }
+    if (pScore > cScore) {
+      result.textContent = "You win the game";
+    }
+    if (cScore > pScore) {
+      result.textContent = "You lose the game";
+    }
+    option.forEach((button) => {
+      button.removeEventListener("click", getPlayerOption);
+    });
   }
+}
 
-      
 function getPlayerOption(e) {
-  console.log('click inside');
-  let playerSelect= (e.target.id);
+  console.log("click inside");
+  let playerSelect = e.target.id;
   playerOption = e.target.textContent;
   game(playerSelect, computerPlay());
-  }
-  
+}
